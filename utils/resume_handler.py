16e57745cd84e56319b6301493496e9a4e3b4416
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-02 07:50:27 +0530

    end app

---
+++ utils/resume_handler.py
@@ -0,0 +1,29 @@
+import os
+from langchain.document_loaders import TextLoader, PyPDFLoader, Docx2txtLoader
+from tempfile import TemporaryDirectory
+import streamlit as st
+
+def get_resume_text(uploaded_file:str)->str:
+    try:
+        with TemporaryDirectory() as tmp_dir:
+            temp_file_path=os.path.join(tmp_dir, uploaded_file.name)
+            
+            with open(temp_file_path, "wb") as resume_file:
+                resume_file.write(uploaded_file.read())
+                
+        upload_document_extension=os.path.splitext(uploaded_file.name)[1]
+        
+        if upload_document_extension==".pdf":
+            loader=PyPDFLoader(temp_file_path)
+        if upload_document_extension==".docx":
+            loader=Docx2txtLoader(temp_file_path)
+        if upload_document_extension==".txt":
+            loader=TextLoader(temp_file_path)
+            
+        if loader:
+            resume_text=loader.load()
+
+            return resume_text
+        
+    except Exception as e:
+        st.error("Error: " + str(e.args))
