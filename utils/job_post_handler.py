41454414d9eefef2809a714f5885d87d80d204b6
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-02 08:22:46 +0530

    add requirements

--- utils/job_post_handler.py
+++ utils/job_post_handler.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def get_job_posts(post_url:str, job_description:str)->str:
     try:
-        if post_url=="":
+        if post_url!="":
             job_post_text=WebBaseLoader(post_url).load()
             return job_post_text
         else:
