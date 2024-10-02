0aec18eda91fdf6b0b43dc73d7f8d4abdc1f5594
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-02 07:57:44 +0530

    add config

--- utils/configuration.py
+++ utils/configuration.py
@@ -3,7 +3,7 @@ import yaml
 from langchain_groq.chat_models import ChatGroq
 import streamlit as st
 
-with open("config.yaml") as file:
+with open("utils/config.yaml") as file:
     config = yaml.safe_load(file)
     
 os.environ['GROQ_API_KEY']=st.secrets['GROQ_API_KEY']
